2e1c84006546b1c01d2daf607e9843442b172a50
Daniel Stepp <willstepp@gmail.com>
2020-10-23 23:45:30 -0400

    config for aws amplify auth, example functions for register, signin, confirm, resetpassword

---
+++ lib/amplifyconfiguration.dart
@@ -0,0 +1,36 @@
+const amplifyconfig = ''' {
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+                "CredentialsProvider": {
+                    "CognitoIdentity": {
+                        "Default": {
+                            "PoolId": "us-east-1:a822b0c4-53cf-4fa0-910c-b6e64f661f88",
+                            "Region": "us-east-1"
+                        }
+                    }
+                },
+                "CognitoUserPool": {
+                    "Default": {
+                        "PoolId": "us-east-1_s1qdi1Sqw",
+                        "AppClientId": "29mq5d8kmq5g9sdtvcou6ac9oe",
+                        "AppClientSecret": "d237m8or3agit1jdish3o5poo4p2hll9p4ibus12v0f7jvfnrup",
+                        "Region": "us-east-1"
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "authenticationFlowType": "USER_SRP_AUTH"
+                    }
+                }
+            }
+        }
+    }
+}''';
--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,230 @@
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:flutter/material.dart';
+import 'package:amplify_core/amplify_core.dart';
+import 'amplifyconfiguration.dart';
+
+import 'dart:developer';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _amplifyConfigured = false;
+  bool isSignUpComplete = false;
+
+  // Instantiate Amplify
+  Amplify amplifyInstance = Amplify();
+
+  @override
+  void initState() {
+    super.initState();
+    _configureAmplify(); 
+  }
+
+  void _configureAmplify() async {
+    if (!mounted) return;
+
+    // Add Cognito Plugins
+    AmplifyAuthCognito authPlugin = AmplifyAuthCognito();
+    amplifyInstance.addPlugin(authPlugins: [authPlugin]);
+
+    // Once Plugins are added, configure Amplify
+    await amplifyInstance.configure(amplifyconfig);
+    try {
+      setState(() {
+        _amplifyConfigured = true;
+      });
+    } catch (e) {
+      print(e);
+    }
+
+  }
+
+  // Send an event to Pinpoint
+  void _recordEvent() async {
+    /*
+    AnalyticsEvent event = AnalyticsEvent("test");
+    event.properties.addBoolProperty("boolKey", true);
+    event.properties.addDoubleProperty("doubleKey", 10.0);
+    event.properties.addIntProperty("intKey", 10);
+    event.properties.addStringProperty("stringKey", "stringValue");
+    Amplify.Analytics.recordEvent(event: event);
+    */
+
+    /*
+    try {
+      Map<String, dynamic> userAttributes = {
+        "email": "willstepp@gmail.com",
+      };
+      SignUpResult res = await Amplify.Auth.signUp(
+        username: "willstepp@gmail.com",
+        password: "palebluedot",
+        options: CognitoSignUpOptions(
+          userAttributes: userAttributes
+        )
+      );
+      setState(() {
+        isSignUpComplete = res.isSignUpComplete;
+        print("SIGNUP COMPLETE!");
+      });
+    } on AuthError catch (e) {
+      print(e.toString());
+    }
+    */
+
+    /*
+    print("EMAIL CONFIRMED: START!");  
+    try {
+      SignUpResult res = await Amplify.Auth.confirmSignUp(
+        username: "willstepp@gmail.com",
+        confirmationCode: "365313"
+      );
+      setState(() {
+        isSignUpComplete = res.isSignUpComplete;
+        print("EMAIL CONFIRMED: ${res.isSignUpComplete}!");
+      });
+    } on AuthError catch (e) {
+      print(e.toString());
+      print(e.cause);
+    }
+
+    try {
+      await Amplify.Auth.signOut();
+      SignInResult res = await Amplify.Auth.signIn(
+        username:"willstepp@gmail.com",
+        password: "palebluedot",
+      );
+      setState(() {
+        print("SIGNED IN (palebluedot): ${res.isSignedIn}!");
+      });
+      try {
+        await Amplify.Auth.updatePassword(
+          newPassword: "silver55",
+          oldPassword: "palebluedot"
+        );
+        await Amplify.Auth.signOut();
+        print("SIGNED OUT");
+        SignInResult res2 = await Amplify.Auth.signIn(
+          username:"willstepp@gmail.com",
+          password: "silver55",
+        );
+        print("SIGNED IN (silver55): ${res2.isSignedIn}!");
+      } on AuthError catch (e) {
+        print(e);
+        print(e.cause);
+      }
+    } on AuthError catch (e) {
+      print(e);
+      print(e.cause);
+    }
+    */
+
+    /*
+    try {
+      ResetPasswordResult res = await Amplify.Auth.resetPassword(
+        username: "willstepp@gmail.com",
+      );
+      setState(() {
+        print("Reset Sent: ${res.isPasswordReset}!");
+      });
+    } on AuthError catch (e) {
+      print(e);
+      print(e.cause);
+    }
+    */
+
+    /*
+    try {
+      await Amplify.Auth.confirmPassword(
+        username: "willstepp@gmail.com",
+        newPassword: "palebluedot",
+        confirmationCode: "238672"
+      );
+      await Amplify.Auth.signOut();
+      print("SIGNED OUT");
+      SignInResult res2 = await Amplify.Auth.signIn(
+        username:"willstepp@gmail.com",
+        password: "palebluedot",
+      );
+      print("SIGNED IN (palebluedot): ${res2.isSignedIn}!");
+    } on AuthError catch (e) {
+      print(e);
+      print(e.cause);
+    }
+    */
+
+    try {
+      /*
+      await Amplify.Auth.signOut();
+      SignInResult res2 = await Amplify.Auth.signIn(
+        username:"willstepp@gmail.com",
+        password: "palebluedot",
+      );
+      print("SIGNED IN (palebluedot): ${res2.isSignedIn}!");
+      */
+      CognitoAuthSession sess = await Amplify.Auth.fetchAuthSession(options: CognitoSessionOptions(getAWSCredentials: true));
+      if (sess.isSignedIn) {
+        print("SIGNED IN");
+        print(sess.userPoolTokens.idToken);
+      } else {
+        print("USER IS NOT SIGNED IN");
+      }
+    } on AuthError catch (e) {
+          print(e);
+          print(e.cause);
+          print(e.exceptionList[0].detail);
+          print(e.exceptionList[1].detail);
+    }
+
+    /*Amplify.Auth.fetchAuthSession().startListening((hubEvent) {
+      switch(hubEvent["eventName"]) {
+        case "SIGNED_IN": {
+          print("USER IS SIGNED IN");
+        }
+        break;
+        case "SIGNED_OUT": {
+          print("USER IS SIGNED OUT");
+        }
+        break;
+        case "SESSION_EXPIRED": {
+          print("USER IS SIGNED IN");
+        }
+        break;
+      }
+    });*/
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Amplify Core example app'),
+          ),
+          body: ListView(padding: EdgeInsets.all(10.0), children: <Widget>[
+            Center( 
+              child: Column (
+                children: [
+                  const Padding(padding: EdgeInsets.all(5.0)),
+                  Text(
+                    _amplifyConfigured ? "configured" : "not configured"
+                  ),                  
+                  RaisedButton(
+                    onPressed: _amplifyConfigured ? _recordEvent : null,
+                    child: const Text('record event')
+                  )
+                ]
+              ),
+            )
+          ])
+      )
     );
   }
 }
