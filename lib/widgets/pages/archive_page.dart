f6ee0bc31021d16593aebee80d1bf7769602e955
Daniel Stepp <willstepp@gmail.com>
2020-11-14 17:51:55 -0500

    added basic upload / thumbnail generation code

--- lib/widgets/pages/archive_page.dart
+++ lib/widgets/pages/archive_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../services/auth/auth_service.dart';
 import './home_page.dart';
+import './upload_page.dart';
 
 class ArchivePage extends StatelessWidget {
   static const routeName = '/archive/index';
@@ -9,12 +10,20 @@ class ArchivePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('~/archive/index'),
+        title: Text('~/archive'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            FlatButton(
+              child: Text('Upload'),
+              color: Colors.green,
+              textColor: Colors.white70,
+              onPressed: () {
+                Navigator.of(context).pushNamed(UploadPage.routeName);
+              },
+            ),
             FlatButton(
               child: Text('Logout'),
               color: Colors.green,
--- lib/widgets/pages/upload_page.dart
+++ lib/widgets/pages/upload_page.dart
@@ -1,20 +1,80 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
-class UploadPage extends StatelessWidget {
+import 'package:image_picker/image_picker.dart';
+import 'package:video_thumbnail/video_thumbnail.dart';
+import 'package:path_provider/path_provider.dart';
+
+class UploadPage extends StatefulWidget {
   static const routeName = '/archive/upload';
+
+  @override
+  _UploadPageState createState() => _UploadPageState();
+}
+
+class _UploadPageState extends State<UploadPage> {
+  File _image;
+  final picker = ImagePicker();
+
+  String _videoPath;
+  String _thumbnailPath;
+
+  FileImage _thumbnailImage;
+
+  Future chooseMomentVideo() async {
+    final pickedFile = await picker.getVideo(source: ImageSource.gallery, maxDuration: const Duration(seconds: 120));
+    if (pickedFile != null) {
+      _image = File(pickedFile.path);
+      _videoPath = pickedFile.path;
+      Directory appDocDir = await getApplicationDocumentsDirectory();
+      String appDocPath = appDocDir.path;
+      _thumbnailPath = await VideoThumbnail.thumbnailFile(
+        video: pickedFile.path,
+        imageFormat: ImageFormat.PNG,
+        thumbnailPath: "$appDocPath/thumbnail.png"
+      );
+      updateThumbnailImage(_thumbnailPath);
+    } else {
+      print('No video selected.');
+    }
+  }
+
+  updateThumbnailImage(String thumbnailPath) {
+    setState(() {
+      _thumbnailImage = new FileImage(File(thumbnailPath)); 
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Upload'),
+        title: Text('~/upload'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'Upload Page',
-            )
+            Text("Video: $_videoPath"),
+            SizedBox(height: 10),
+            SizedBox(
+              width: 320,
+              height: 240,
+              child: DecoratedBox(
+                position: DecorationPosition.foreground,
+                decoration: BoxDecoration(
+                  image: _thumbnailImage != null ? DecorationImage(image: _thumbnailImage) : null
+                ),
+              )
+            ),
+            SizedBox(height: 10),
+            FlatButton(
+              child: Text('Choose'),
+              color: Colors.green,
+              textColor: Colors.white70,
+              onPressed: chooseMomentVideo,
+            ),
           ]
         )
       )
